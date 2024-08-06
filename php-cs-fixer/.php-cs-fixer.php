3e3f81913c4e7059640bb226b6c8772669194db0
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2024-08-06 14:08:49 +1000

    (php-cs-fixer) adjust paths

--- php-cs-fixer/.php-cs-fixer.php
+++ php-cs-fixer/.php-cs-fixer.php
@@ -4,8 +4,8 @@
  */
 $finder = PhpCsFixer\Finder::create()
             ->in([
-                __DIR__ . '/src',
-                __DIR__ . '/tests',
+                'src',
+                'tests',
             ]);
 
 $config = new PhpCsFixer\Config();
