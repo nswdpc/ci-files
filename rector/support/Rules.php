5e4f3ed409ac98aab7d65722ab03b4ae0520eced
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2025-04-07 16:13:00 +1000

    Remove rules added to skip

--- rector/support/Rules.php
+++ rector/support/Rules.php
@@ -54,11 +54,7 @@ abstract class Rules {
             \Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector::class,
 
             // Avoid applying this rule, not handling ?SomeClass return types appropriately
-            \Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector::class,
-
-            // These rules causing issues with class extends/inheritance
-            \Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector::class,
-            \Rector\Renaming\Rector\Name\RenameClassRector::class
+            \Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector::class
 
         ];
     }
