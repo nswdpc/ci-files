87aa495f80064e691509cb10bacc50ca8b498e3c
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2025-04-16 10:32:49 +1000

    Provide paths via the command for more flexibility

--- php-cs-fixer/.php-cs-fixer.php
+++ php-cs-fixer/.php-cs-fixer.php
@@ -3,10 +3,7 @@
  * Configuration file for https://github.com/PHP-CS-Fixer/PHP-CS-Fixer
  */
 $finder = PhpCsFixer\Finder::create()
-            ->in([
-                'src',
-                'tests',
-            ]);
+            ->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
