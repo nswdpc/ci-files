0c1643dee48aa7ba450020894e204ab6c2eda8c9
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2025-06-16 15:10:34 +1000

    Initial v4 update

--- README.md
+++ README.md
@@ -1,17 +1,46 @@
 #  CI Files
 
-A collection of files for use in CI and development.
+A collection of files and configuration for use in CI and development.
 
-The workflows will improve code quality using `friendsofphp/php-cs-fixer` and automated code upgrades using `rector/rector`.
+* Code standards: `friendsofphp/php-cs-fixer`
+* Static analyis: `phpstan/phpstan` and
+* Automated code refactoring: `rector/rector`
 
 ## Install
 
 Require as a dev component in your project/module:
 
 ```sh
-composer require --dev nswdpc/ci-files:^3
+composer require --dev nswdpc/ci-files:^4
 ```
+
+Use the relevant tag or dev-v-N branch as a dev requirement, depending on your refactoring requirements.
 
 ## Use
 
 [Read the documentation](./docs/en/001_index.md)
+
+
+## License
+
+[BSD-3-Clause](./LICENSE.md)
+
+## Maintainers
+
++ PD web team
+
+## Bugtracker
+
+We welcome bug reports, pull requests and feature requests on the Github Issue tracker for this project.
+
+Please review the [code of conduct](./code-of-conduct.md) prior to opening a new issue.
+
+## Security
+
+If you have found a security issue with this module, please email digital[@]dpc.nsw.gov.au in the first instance, detailing your findings.
+
+## Development and contribution
+
+If you would like to make contributions to the module please ensure you raise a pull request and discuss with the module maintainers.
+
+Please review the [code of conduct](./code-of-conduct.md) prior to completing a pull request.
---
+++ rector/.rector.silverstripe_6_84.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Example commands:
+ * add -n for --dry-run
+ * use -h for help docs
+ * Standard: ./vendor/bin/rector process -c .rector.dist.php vendor/vendorname/module
+ * With no diff: ./vendor/bin/rector process --no-diffs -c .rector.dist.php vendor/vendorname/module
+ */
+
+// common Silverstripe bootstrap
+require_once(__DIR__ . '/support/SilverstripeBootstrap.php');
+
+// common rules handling class
+require_once(__DIR__ . '/support/Rules.php');
+
+// @var \Rector\Configuration\RectorConfigBuilder $builder
+$builder = \Rector\Config\RectorConfig::configure();
+return $builder
+
+    ->withBootstrapFiles([
+        __DIR__ . '/bootstrap/silverstripe.php',
+    ])
+
+    ->withPaths([
+        __DIR__
+    ])
+
+    ->withSkip(
+        \NSWDPC\Rector\Rules::mergeSkipRules([])
+    )
+
+    ->withRules(
+        \NSWDPC\Rector\Rules::commonRules()
+    )
+
+    ->withConfiguredRule(\Rector\Strict\Rector\If_\BooleanInIfConditionRuleFixerRector::class, [
+        'treat_as_non_empty' => true
+    ])
+
+    ->withSets([
+        \Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_60,
+        \Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList::CODE_QUALITY
+    ])
+
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        earlyReturn: false,
+        instanceOf: true,
+        typeDeclarations: true,
+        naming: false,
+        phpunit: false,
+        strictBooleans: true
+    )
+
+    ->withPhpSets(
+        php84: true
+    );
